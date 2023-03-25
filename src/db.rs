27a6133841633a2bb4cf0d5f2796811a89c73a83
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2023-03-25 17:02:20 -0500

    feat: add functions to test the db

--- src/db.rs
+++ src/db.rs
@@ -1,3 +1,5 @@
+use std::path::{Path, PathBuf};
+
 use rusqlite::{Connection, Error};
 
 use crate::utils::directory::Directory;
@@ -8,6 +10,26 @@ impl Db {
     pub fn open() -> Result<Db, Error> {
         let mut local_db = Directory::get_local_dir("db").unwrap();
         local_db.push("flow.sqlite");
+        Self::open_db(local_db)
+    }
+
+    #[allow(dead_code)]
+    pub fn open_test_db() -> Result<Db, Error> {
+        Self::open_db(Self::get_path_test_db())
+    }
+
+    /// Get the test-db path
+    #[allow(dead_code)]
+    pub fn get_path_test_db() -> PathBuf {
+        let mut local_db = Directory::get_local_dir("test_db").unwrap();
+        local_db.push("flow-test-db.sqlite");
+        local_db
+    }
+
+    pub fn open_db<P>(local_db: P) -> Result<Db, Error>
+    where
+        P: AsRef<Path>,
+    {
         let conn = Connection::open(local_db)?;
         conn.execute(
             "
