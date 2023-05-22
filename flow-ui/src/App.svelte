d31ed9e7bc8229cf18187c48f8df990db79d325f
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2023-05-22 15:56:18 -0500

    feat: create the basic layout

---
+++ flow-ui/src/App.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import SystemUi from "./system-ui.svelte";
+  import Switch from "./components/switch.svelte";
+  import { SunIcon, MoonIcon } from "./components/icons";
+</script>
+
+<SystemUi>
+  <nav
+    class="flex flex-row items-center justify-between py-2 px-3 bg-soft rounded-lg"
+  >
+    <div class="nav-logo">
+      <img src="/images/flow-logo.png" alt="Flow Logo" />
+    </div>
+    <ul class="flex flex-row items-center">
+      <Switch checked let:checked>
+        {#if checked}
+          <MoonIcon class="fill-zinc-600 w-10/12" />
+        {:else}
+          <SunIcon class="fill-zinc-600 w-10/12" />
+        {/if}
+      </Switch>
+    </ul>
+  </nav>
+</SystemUi>
+
+<style>
+  .nav-logo {
+    width: 70px;
+  }
+
+  nav {
+    border: 1px solid white;
+    min-height: 70px;
+    width: 100%;
+  }
+</style>
