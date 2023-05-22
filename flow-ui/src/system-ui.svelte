a5c4d63305a33df70155986defbc3a35afba2d4a
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2023-05-22 15:54:55 -0500

    fix: add css variables using svelte component

---
+++ flow-ui/src/system-ui.svelte
@@ -0,0 +1,17 @@
+<slot />
+
+<style>
+  :global(:root) {
+    --alpha: 1;
+  }
+
+  :global(.light-theme) {
+    --bg-strong: 233, 67%, 98%;
+    --bg-soft: 44, 0%, 100%;
+  }
+
+  :global(.dark-theme) {
+    --bg-back: 253, 5%, 26%;
+    --bg-soft: 217, 20%, 98%;
+  }
+</style>
