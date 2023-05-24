4e8a0fa86e1f652e922692d580b196d709b2185e
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2023-05-24 17:12:50 -0500

    style: add color texts

--- flow-ui/src/system-ui.svelte
+++ flow-ui/src/system-ui.svelte
@@ -6,11 +6,14 @@
   }
 
   :global(.light-theme) {
+    --text-strong: 215, 5%, 18%;
+    --text-soft: 232, 9%, 52%;
     --bg-strong: 233, 67%, 98%;
     --bg-soft: 44, 0%, 100%;
   }
 
   :global(.dark-theme) {
+    --text-strong: 215°, 5%, 18%;
     --bg-back: 253, 5%, 26%;
     --bg-soft: 217, 20%, 98%;
   }
