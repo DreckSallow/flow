3af3a2a2ee4f536e2ad1f090feecd740fde58a52
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2023-05-26 09:32:32 -0500

    feat: add task details modal

--- flow-ui/src/components/icons/index.ts
+++ flow-ui/src/components/icons/index.ts
@@ -1,2 +1,3 @@
-export {default as SunIcon} from "./sun.svelte"
-export {default as MoonIcon} from "./moon.svelte"
+export { default as SunIcon } from "./sun.svelte";
+export { default as MoonIcon } from "./moon.svelte";
+export { default as CloseIcon } from "./close.svelte";
